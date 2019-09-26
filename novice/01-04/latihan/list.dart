6d4252af63156775a630b79ace9096ffb6cd7c77
Ahnaf <ahnafalfariza@outlook.com>
2019-09-26 15:25:16 +0700

    done latihan day 4

---
+++ novice/01-04/latihan/list.dart
@@ -0,0 +1,13 @@
+main(){
+  //Declaring list
+  var list = new List(5);
+  list[0]=1;
+  list[2]=10;
+
+  var listInt = [3,4,5,6];
+  var listString = ["Ahnaf", "Muh", "Alf"];
+
+  print(list);
+  print(listInt);
+  print(listString);
+}
---
+++ novice/01-04/latihan/sets.dart
@@ -0,0 +1,10 @@
+main(){
+  var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
+  print(halogens);
+
+  var name = <String> {};
+  name.add("Ahnaf");
+  name.add("Muh");
+  name.add("Alf");
+  print(name);
+}
